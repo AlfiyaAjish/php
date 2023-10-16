4de918945d034c4709dc022a7385c830b253d758
AlfiyaAjish <147368676+AlfiyaAjish@users.noreply.github.com>
2023-10-16 11:48:34 +0530

    Add files via upload

--- form1.php
+++ form1.php
@@ -3,11 +3,11 @@
                 <body>
                    <form action="form1.php" method="post"><pre>
                           Name:    <input type="text" name="a"><br>
-                          Address: <textarea type="text" placeholder="enter"></textarea><br>
-                          DOB:     <input type="date" ><br>
+                          Address: <textarea type="text" placeholder="enter" name="b"></textarea><br>
+                          DOB:     <input type="date" name="c"><br>
                           password:<input type="password"><br>
-                                   <input type="submit" name="n1" value="submit">
-                           <a href="table.html"><input type="button" value="back"></a>        
+                                   <input type="submit" name="n1" value="submit">   
+                                    <input type="submit" name="n2" value="reset">     
   			   </pre>	                 
                    </form>
                    
@@ -17,6 +17,14 @@
 if(isset($_POST['n1']))
 {
 $a=$_POST['a'];
+$b=$_POST['b'];
+$c=$_POST['c'];
 echo "the value you entered=",$a;
+echo "the value you entered=",$b; 
+echo "the value you entered=",$c; 
+}
+if(isset($_POST['n2']))
+{
+header(Refresh);
 }
 ?>               
